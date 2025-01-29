2aab84aefe230767b8f4ca1a2d20ada7980b1dbc
SAURABH <saurabh@192.168.1.30>
2025-01-29 14:07:15 +0545

    javascript new file

---
+++ index.js
@@ -0,0 +1,12 @@
+// let a= 5
+// let b=5
+// let add = a+b
+// console.log("Answer of Addition is",add)
+
+let  number = 5
+if (number >0 ){
+    console.log(number,' is positive')
+}
+else (
+    console.log(number,' is negative')
+)
