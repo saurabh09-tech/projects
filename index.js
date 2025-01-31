1a0ab397bdfa5bca6ff7042ea6d100d14bcfc393
SAURABH <saurabh@192.168.1.35>
2025-01-31 14:05:18 +0545

    update function

--- index.js
+++ index.js
@@ -3,10 +3,53 @@
 // let add = a+b
 // console.log("Answer of Addition is",add)
 
-let  number = 5
-if (number >0 ){
-    console.log(number,' is positive')
-}
-else (
-    console.log(number,' is negative')
-)   
+// 
+// 
+// function checkpositive (number){
+//     if (number>0){
+//         console.log (number,"is positive")
+//     }
+//     else {
+//         console.log(number,"is negative")
+//     }
+// }
+
+// checkpositive(10)
+// checkpositive(-5)
+   
+// function converttosecond(min){
+//     let second=min*60
+//     return second
+
+// }
+// let second= converttosecond(2)
+// console.log(second,'seconds')
+
+// function writtennextnumber (initialnumber)
+// {
+//     let nextnumber= initialnumber +1
+// return nextnumber
+// }
+// let newnumber=writtennextnumber(5)
+// console.log("Next number is ",newnumber)
+function Agetodays(Age){
+        let days =Age*365
+        return days
+    
+    }
+    let days= Agetodays(1)
+    console.log(days,'days')
+
+    function numbers (a,b) {
+        if (a>b){
+            return a
+        }
+        else{
+            return b
+        }
+    }
+       
+    let greatest=numbers(2,3)
+    console.log(greatest,"is greatest number")
+
+    
