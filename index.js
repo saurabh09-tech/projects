369056c54c97905361fd695003c57cdcd46668ff
SAURABH <saurabh@192.168.1.43>
2025-02-02 14:08:33 +0545

    update javascript

--- index.js
+++ index.js
@@ -32,24 +32,48 @@
 // }
 // let newnumber=writtennextnumber(5)
 // console.log("Next number is ",newnumber)
-function Agetodays(Age){
-        let days =Age*365
-        return days
+// function Agetodays(Age){
+//         let days =Age*365
+//         return days
     
-    }
-    let days= Agetodays(1)
-    console.log(days,'days')
+//     }
+//     let days= Agetodays(1)
+//     console.log(days,'days')
 
-    function numbers (a,b) {
-        if (a>b){
-            return a
-        }
-        else{
-            return b
-        }
-    }
+//     function numbers (a,b) {
+//         if (a>b){
+//             return a
+//         }
+//         else{
+//             return b
+//         }
+//     }
        
-    let greatest=numbers(2,3)
-    console.log(greatest,"is greatest number")
+//     let greatest=numbers(2,3)
+//     console.log(greatest,"is greatest number")
 
-    
+// function number(a,b){
+//     if (a<b){
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+// }
+// let smallestnumber= number(3,4);
+// console.log(smallestnumber,"is smallest number");
+
+// let name="saurabh"
+// for (i=1;i<=10;i++)
+// {
+//     console.log(name)
+// }
+   
+// 
+
+let names=['Nirajan','Abishek','Bikash','Hari','Shyam','Madan','Krishna','Sachin','Dipesh','Rahul','Ganesh']
+names.push('Manish')
+console.log(names)
+names.shift()
+
+
